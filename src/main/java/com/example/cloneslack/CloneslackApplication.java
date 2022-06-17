d244e3b005a75e021d032f8c245feef8fe546dac
VinceRF <zhsk30@naver.com>
2022-06-17 10:23:15 +0900

    클론프로젝트 생성

---
+++ src/main/java/com/example/cloneslack/CloneslackApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloneslack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneslackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneslackApplication.class, args);
+    }
+
+}
