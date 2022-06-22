4fcbeb07d8a499110e559a516b82415fd4db9c71
VinceRF <zhsk30@naver.com>
2022-06-22 23:09:47 +0900

    메인 함수 리뉴얼

--- src/main/java/com/example/cloneslack/CloneslackApplication.java
+++ src/main/java/com/example/cloneslack/CloneslackApplication.java
@@ -5,6 +5,7 @@ import com.example.cloneslack.repository.ChatRoomRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -12,9 +13,19 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class CloneslackApplication {
 
+
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.properties";
+
     public static void main(String[] args) {
-        SpringApplication.run(CloneslackApplication.class, args);
+        new SpringApplicationBuilder(CloneslackApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
+
     }
+//    public static void main(String[] args) {
+//        SpringApplication.run(CloneslackApplication.class, args);
+//    }
 
     @Bean
     public CommandLineRunner demo(ChatRoomRepository repository){
